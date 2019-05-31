e1ba8a582124aac2ecc64baaf41ca441268b4d9a
Junjun.Li <809030512@qq.com>
2019-05-31 10:30:39 +0800

    Update AutoSequence.py

--- AutoSequence.py
+++ AutoSequence.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import time
+import traceback
 from Python_CANoe import CANoe
 
 
@@ -20,6 +21,19 @@ def get_sequences(auto_folder):
                 sequences_list.append(os.path.join(root, file_name))
     return sequences_list
 
+def rename_log(log_folder, rename_name):
+    filenames = os.listdir(log_folder)
+    _name = []
+    _time = []
+
+    for filename in filenames:
+        if filename[-4:] == '.asc':
+            c_time = os.path.getctime(log_folder + os.sep + filename)
+
+            _name.append(log_folder + os.sep + filename)
+            _time.append(c_time)
+    newest_log = _name[_time.index(max(_time))]
+    os.rename(newest_log, log_folder + os.sep + rename_name + "_" + os.path.basename(newest_log))
 
 
 class VSQ:
@@ -28,21 +42,34 @@ class VSQ:
         self.vsq_lines = []
         self.can_app = CANoe()
 
-        self.can_app.open_simulation("test.cfg")
+        self.can_app.open_simulation("C:\\Data\\Users\\li_j11\\Profil\\Desktop\\Temp\\new CG2474\\20_20_137_new\\CG2474_0517.cfg")
+        self.log_folder = "C:\\Data\\Users\\li_j11\\Profil\\Desktop\\Temp\\new CG2474\\Logging"
 
     def read_vsq(self):
         for vsq in self.vsq_list:
             print("################ Test %s" % vsq)
+
+            # print(os.path.basename(vsq))
+            # return
             # Todo: 启动CANoe
             self.can_app.start_Measurement()
 
             with open(vsq, 'r') as file_to_read:
                 self.vsq_lines = file_to_read.readlines()
                 while self.vsq_lines:
-                    self.parse_vsq(False, 0)
+                    try:
+                        self.parse_vsq(False, 0)
+                    except Exception as error:
+                        print(traceback.format_exc())
+                        self.can_app.stop_Measurement()
+                        break
             
+            # Stop measurement and rename log file
             self.can_app.stop_Measurement()
-            time.sleep(20)
+            time.sleep(10)
+            rename_name = os.path.basename(vsq).replace(".vsq", "")
+            rename_log(self.log_folder, rename_name)
+
 
     def parse_vsq(self, repeat_indecator, index):
         line = self.vsq_lines[index].strip()
@@ -65,7 +92,13 @@ class VSQ:
             ns_name = sys_var[1]
             sysvar_name = sys_var[2]
             set_value = line_list[4]
-            
+
+            value_type = sys_var[-1]
+            if value_type == "Integer":
+                set_value = int(set_value)
+            elif value_type == "Float":
+                set_value = float(set_value)
+            # print(ns_name, sysvar_name, set_value)
             self.can_app.set_SysVar(ns_name, sysvar_name, set_value)
             
             # Todo: CANoe中设置sysvar
@@ -93,7 +126,8 @@ class VSQ:
 
         else:
             print("************************", line, "*******************")
-            return 1
+            raise Exception("Invalid commad: %s" % line)
+            
 
 if __name__ == "__main__":
     sequences_list = get_sequences("Auto_Sequences")
